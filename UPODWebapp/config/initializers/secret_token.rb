cc4f8e2332bbc470da4ae3ebc415fbb500a54db5
Sameen Jalal <sameenjalal@gmail.com>
2011-10-22 09:09:38 -0700

    Added rails folder

---
+++ UPODWebapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UPODWebapp::Application.config.secret_token = 'c22cb6dd53477bcba86616fce06ffe99ed431809ee6012664f7cdebc6aaeec2c5688de0400f7c54dd0ce292e7fe21d5188bbd0b45a1532dbf7c5a79922084849'
